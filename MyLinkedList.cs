c83109d6c23446bb05e00064631c58435c47eb9e
JZheng <6043508@mborijnland.nl>
2025-09-09 09:51:47 +0200

    working

--- MyLinkedList.cs
+++ MyLinkedList.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 2. Array, List
+// 3. Zodat je elementen kan opzoeken
+// 4. je kan elk type lijst maken die je maar wilt
+
 namespace CsOpdrachten
 {
     public class MyLinkedList<T> : IMyList<T>
@@ -27,9 +31,8 @@ namespace CsOpdrachten
 
                 Node current = _head;
                 for (int i = 0; i < index; i++)
-                {
                     current = current.Next!;
-                }
+
                 return current.Value;
             }
             set 
@@ -39,14 +42,13 @@ namespace CsOpdrachten
 
                 Node current = _head;
                 for (int i = 0; i < index; i++)
-                {
                     current = current.Next!;
-                }
+
                 current.Value = value;
             }
         }
 
-        public void Clear()
+        public void Clear()  //O(1)
         {
             //not pointing to any nodes... so... it's empty right?
             _head = null;
@@ -74,21 +76,80 @@ namespace CsOpdrachten
                 _tail!.Next = newNode; //the (old) _tail.Next points to the newNode with the element
                 _tail = newNode;      //changeing tail does not change the .Next of the old _tail
             }
-            _count++; //for quick returning of the length 
+            _count++; //for quick returning of the length of list
         }
             
+        public int IndexOf(T element) //O(n)
+        {
+            if (_head is null)
+                throw new NullReferenceException("List is empty");
 
-        public int IndexOf(T element) => //O()
-            throw new NotImplementedException();
+            Node? current = _head;
 
+            for (int i = 0; i < Count(); i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(current!.Value, element))
+                    return i;
 
-        public bool Contains(T element) => //O()
-            throw new NotImplementedException();
+                current = current.Next;
+            }
+            //returns -1 if the element isn't in the list
+            return -1;
+        }
 
-        public void Insert(int index, T element) => //O()
-            throw new NotImplementedException();
+        public bool Contains(T element) //O(n)
+        {
+            if (_head is null)
+                throw new NullReferenceException("List is empty");
 
-        public void Remove(T element)
+            Node? current = _head;
+            //loop through, return if found
+            for (int i = 0; i < _count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(current!.Value, element))
+                    return true;
+
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Insert(int index, T element) //O(n)
+        {
+            if (_head is null)
+                throw new NullReferenceException("List is empty");
+
+            if (index < 0 || index > _count)
+                throw new NullReferenceException("Index out of range");
+
+            Node? current = _head;
+            Node? previous = null;
+
+            Node newNode = new()
+            {
+                Value = element
+            };
+
+            //front
+            if (index == 0)
+            {
+                newNode.Next = current;
+                _head = newNode;
+            }
+            else
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    previous = current;
+                    current = current!.Next;
+                }
+                previous!.Next = newNode;
+                newNode.Next = current;
+            }
+            _count++;
+        }
+
+        public void Remove(T element) //O(n)
         {
             if (_head is null)
                 throw new InvalidOperationException("List is empty!");
@@ -118,11 +179,34 @@ namespace CsOpdrachten
             }
         }
 
-        public void RemoveAt(int index) =>
-            throw new NotImplementedException();
+        public void RemoveAt(int index) //O(n)
+        {
+            if (_head is null)
+                throw new NullReferenceException("List is empty");
+
+            Node? current = _head;
+            Node? previous = null;
+
+            if (index == 0) 
+            {
+                _head = current.Next;
+                _count--;
+                return;
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                previous = current;
+                current = current!.Next;
+            }
+
+            previous!.Next = current!.Next;
+            _count--;
+        }
 
         public int Count() => //O(1)
             _count;
 
     }
 }
+
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -43,9 +43,9 @@ namespace CsOpdrachten
             list.Add(3);
 
             list.Remove(3);
+
             list[0].Should().Be(1);
             list[1].Should().Be(2);
-
             list.Count().Should().Be(3);
         }
 
@@ -58,6 +58,7 @@ namespace CsOpdrachten
             list.Add(2);
 
             list.Remove(2);
+
             list[0].Should().Be(1);
             list[1].Should().Be(3);
             list[2].Should().Be(2);
@@ -70,8 +71,147 @@ namespace CsOpdrachten
             list.Add(2);
             list.Add(1);
             list.Add(3);
+
             list.Clear();
+
             list.Count().Should().Be(0);
+        }
+
+        [Test]
+        public void Removing_an_element_at_index_should_remove_it_and_resize_the_list()
+        {
+            list.Add(2);
+            list.Add(1);
+            list.Add(3);
+            list.Add(2);
+
+            list.RemoveAt(2);
+
+            list[0].Should().Be(2);
+            list[1].Should().Be(1);
+            list[2].Should().Be(2);
+            list.Count().Should().Be(3);
+        }
+
+        [Test]
+        public void Removing_the_first_index_with_RemoveAt_should_work_properly()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            list.RemoveAt(0);
+
+            list[0].Should().Be(2);
+            list[1].Should().Be(3);
+            list.Count().Should().Be(2);
+        }
+
+        [Test]
+        public void Removing_the_last_element_should_not_cause_problems_either()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            list.RemoveAt(2);
+
+            list[0].Should().Be(1);
+            list[1].Should().Be(2);
+
+            Action act = () => { var _ = list[2]; };
+            act.Should().Throw<NullReferenceException>();
+        }
+
+        [Test]
+        public void Inserting_an_element_at_the_front_should_add_it_to_the_front()
+        {
+            list.Add(2);
+            list.Add(3);
+
+            list.Insert(0, 1);
+
+            list[0].Should().Be(1);
+            list[1].Should().Be(2);
+            list.Count().Should().Be(3);
+        }
+
+        [Test]
+        public void Inserting_an_element_in_the_center_should_add_it()
+        {
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+
+            list.Insert(1, 2);
+
+            list[0].Should().Be(1);
+            list[1].Should().Be(2);
+            list[2].Should().Be(3);
+            list.Count().Should().Be(4);
+        }
+
+        [Test]
+        public void Inserting_an_element_in_the_last_position_should_insert_it_between_the_last_and_one_before_last()
+        {
+            list.Add(1);
+            list.Add(3);
+
+            list.Insert(1, 2);
+
+            list[0].Should().Be(1);
+            list[1].Should().Be(2);
+            list[2].Should().Be(3);
+        }
+
+        [Test]
+        public void Insert_should_throw_error_if_argument_goes_out_of_range()
+        {
+            list.Add(1);
+            list.Add(3);
+
+            Action act = () => list.Insert(-1, 3);
+            act.Should().Throw<NullReferenceException>();
+
+            act = () => list.Insert(4, 3);
+            act.Should().Throw<NullReferenceException>();
+        }
+
+        [Test]
+        public void Contains_should_return_true_if_element_is_in_list()
+        {
+            list.Add(1);
+            list.Contains(1).Should().BeTrue();
+        }
+
+        [Test]
+        public void Contains_should_return_false_if_element_is_not_in_list()
+        {
+            list.Add(1);
+            list.Contains(2).Should().BeFalse();
+        }
+
+        [Test]
+        public void Contains_should_throw_nullreference_if_list_is_empty()
+        {
+            Action act = () => list.Contains(1);
+            act.Should().Throw<NullReferenceException>();
+        }
+
+        public void IndexOf_should_return_the_index_of_element_in_list()
+        {
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+
+            list.IndexOf(1).Should().Be(0);
+            list.IndexOf(4).Should().Be(4);
+            list.IndexOf(3).Should().Be(1);
+        }
+
+        public void IndexOf_should_return_minus_one_if_the_element_is_not_in_list()
+        {
+            list.IndexOf(2).Should().Be(-1);
         }
     }
 }
