6164bdbd788d83f75ee95d6d84346de8f0e752be
JZheng <6043508@mborijnland.nl>
2025-09-16 10:38:39 +0200

    linq

---
+++ Extensions.cs
@@ -0,0 +1,125 @@
+﻿using CsOpdrachten;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsOpdrachten
+{
+    public static class Extensions
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(selector);
+
+            foreach (var i in source)
+                yield return selector(i);
+        }
+
+        public static IEnumerable<TSource> MyWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+
+            foreach (var i in source)
+            {
+                if (predicate(i))
+                    yield return i;
+            }
+        }
+
+        public static int MyCount<TSauce>(this IEnumerable<TSauce> source, Func<TSauce, bool> predicate)
+        {
+
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+
+            int count = 0;
+
+            foreach (var i in source)
+            {
+                if (predicate(i))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int MyCount<TSource> (this IEnumerable<TSource> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            int count = 0;
+            foreach (var i in source)
+                count++;
+
+            return count;
+        }
+
+        public static bool MyAny<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(predicate);
+
+            foreach (var i in source)
+            {
+                if (predicate(i))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool MyAny<TSource>(this IEnumerable<TSource> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            using var enumerator = source.GetEnumerator();
+            return enumerator.MoveNext();
+        }
+        
+
+        public static TSource? MyFirstOrDefault<TSource>(this IEnumerable<TSource> source)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            foreach (var i in source)
+                return i;
+            return default;
+        }
+
+        public static TSource? MyFirstOrDefault<TSource>(this IEnumerable<TSource> source, TSource value)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            foreach (var i in source)
+                return i;
+            return value;
+        }
+
+        public static TSource? MyFirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            foreach (var i in source)
+                if (predicate(i))
+                    return i;
+
+            return default;
+        }
+
+        public static TSource? MyFirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, TSource value)
+        {
+            ArgumentNullException.ThrowIfNull(source);
+
+            foreach (var i in source)
+                if (predicate(i))
+                    return i;
+
+            return value;
+        }
+    }
+}
+
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using AwesomeAssertions;
+using AwesomeAssertions.Specialized;
+using System;
 using System.Diagnostics;
 
 namespace CsOpdrachten
@@ -12,6 +14,8 @@ namespace CsOpdrachten
         {
             list = new MyLinkedList<int>();
         }
+
+        #region Opracht1
 
         [Test]
         public void Adding_an_element_will_add_it_to_the_end_of_list()
@@ -253,9 +257,12 @@ namespace CsOpdrachten
         public void IndexOf_throws_invalid_operation_if_list_is_null()
         {
             Action act = () => list.IndexOf(2);
-            act.Should().Throw < InvalidOperationException>();
+            act.Should().Throw<InvalidOperationException>();
         }
-        
+        #endregion
+
+        #region Enumerator
+
         [Test]
         public void Checks_the_enumerator_output()
         {
@@ -277,7 +284,7 @@ namespace CsOpdrachten
         }
 
         [Test]
-        public void Enumerator_move_next_should_be_false_when_list_is_empty() 
+        public void Enumerator_move_next_should_be_false_when_list_is_empty()
         {
             var enumerator = list.GetEnumerator();
             enumerator.MoveNext().Should().BeFalse();
@@ -291,12 +298,462 @@ namespace CsOpdrachten
             list.Add(3);
             list.Add(4);
 
-            List<int> newList = [] ;
+            List<int> newList = [];
 
             foreach (var i in list)
                 newList.Add(i);
 
             newList.Should().ContainInOrder(1, 2, 3, 4);
+        }
+        #endregion
+
+
+        #region Extensions
+
+        [Test]
+        public void Where_should_return_an_IEnumerable_with_elements_that_are_true()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            var even = list.MyWhere(n => n % 2 == 0).ToList();
+
+            even[0].Should().Be(2);
+            even[1].Should().Be(4);
+        }
+
+        [Test]
+        public void MyWhere_with_null_source_should_throw_exception_when_trying_to_enumerate_it()
+        {
+            List<int>? nums = null;
+
+            Action act = () => { var _ = nums!.MyWhere(n => n % 2 == 0).ToList(); };
+
+            act.Should().Throw<ArgumentNullException>();
+
+        }
+
+        [Test]
+        public void MyWhere_with_null_predicate_should_throw_exception_when_trying_to_enumerate()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+
+            Func<int, bool>? predicate = null;
+            Action act = () => list.MyWhere(predicate!).ToList();
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyWhere_should_return_empty_if_nothing_matches_the_predicate()
+        {
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+
+            var empty = list.MyWhere(x => x % 2 == 0).ToList();
+
+            empty.Should().BeEmpty();
+        }
+
+        [Test]
+        public void MyWhere_should_return_empty_if_the_list_is_empty()
+        {
+            var empty = list.MyWhere(x => x > 0).ToList();
+            empty.Should().BeEmpty();
+        }
+
+        [Test]
+        public void Mywhere_enumerator_should_enumerate_properly()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            var toThree = list.MyWhere(x => x < 4).ToList();
+            var enumerator = toThree.GetEnumerator();
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(1);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(2);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(3);
+
+            enumerator.MoveNext().Should().BeFalse();
+        }
+
+        [Test]
+        public void MySelect_source_if_null_should_throw_exception_when_enumerating()
+        {
+            List<int>? nums = null;
+            Action act = () => { var _ = nums!.MySelect(n => n.ToString()).ToList(); };
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MySelect_selector_if_null_should_throw_exception_when_enumerating()
+        {
+            Func<int, int>? no = null;
+            Action act = () => list.MySelect(no!).ToList();
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MySelect_should_return_empty_if_list_is_empty()
+        {
+            list.MySelect(n => n.ToString());
+            list.Should().BeEmpty();
+        }
+
+        [Test]
+        public void MySelect_should_return_ints_as_strings()
+        {
+            string[] stringnums = ["1", "2", "3"];
+
+            List<int> nums = stringnums.MySelect(n => Convert.ToInt32(n)).ToList();
+            foreach (var i in nums)
+            {
+                i.Should().BeOfType(typeof(int));
+            }
+        }
+
+        [Test]
+        public void MySelect_enumerator_should_enumerate_properly()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            var toString = list.MySelect(n => n.ToString()).ToList();
+            var enumerator = toString.GetEnumerator();
+
+            enumerator.Current.Should().Be(null);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be("1");
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be("2");
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be("3");
+
+            enumerator.MoveNext().Should().BeFalse();
+            toString.Count.Should().Be(3);
+        }
+
+
+        [Test]
+        public void MyCount_source_if_null_should_throw_exception()
+        {
+            List<int>? nums = null;
+            Action act = () => { var _ = nums!.MyCount(n => n > 0); };
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyCount_predicate_if_null_should_throw_exception()
+        {
+            Func<int, bool>? no = null;
+            Action act = () => list.MyCount(no!);
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyCount_source_without_predicacte_if_null_should_throw_exception()
+        {
+            List<int>? nums = null;
+            Action act = () => { var _ = nums!.MyCount(); };
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void If_there_are_no_elements_count_should_be_zero()
+        {
+            int count = list.MyCount();
+            count.Should().Be(0);
+        }
+
+        [Test]
+        public void MyCount_should_return_count_of_elements()
+        {
+            list = [1, 2, 3];
+            int count = list.MyCount();
+            count.Should().Be(3);
+        }
+        [Test]
+        public void MyCount_should_return_count_of_elements_matching_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            int count = list.MyCount(n => n % 2 == 0);
+            count.Should().Be(2);
+        }
+
+        [Test]
+        public void If_nothing_matches_predicate_count_should_be_zero()
+        {
+            int matches = list.MyCount(n => n > 0);
+            matches.Should().Be(0);
+        }
+
+
+        [Test]
+        public void MyAny_source_if_null_should_throw_exception_when_enumerating()
+        {
+            List<int>? nums = null;
+            Action act = () => nums!.MyAny(n => n > 0);
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyAny_predicate_if_null_should_throw_exception_when_enumerating()
+        {
+            Func<int, bool>? no = null;
+            Action act = () => list.MyAny(no!);
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyAny_should_return_false_if_none_of_the_elements_fit_the_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            bool containsNegative = list.MyAny(n => n < 0);
+
+            containsNegative.Should().BeFalse();
+        }
+
+
+        [Test]
+        public void MyAny_should_return_true_if_any_of_the_elements_fit_the_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(-1);
+
+            bool containsNegative = list.MyAny(n => n < 0);
+
+            containsNegative.Should().BeTrue();
+        }
+
+        [Test]
+        public void MyAny_should_return_false_if_the_list_is_empty_with_predicates()
+        {
+            bool containsNegative = list.MyAny(n => n < 0);
+            containsNegative.Should().BeFalse();
+        }
+
+        #endregion
+
+        [Test]
+        public void MyAny_enumerator()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(-1);
+            var enumerator = list.GetEnumerator();
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(1);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(2);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(-1);
+
+            enumerator.MoveNext().Should().BeFalse();
+
+        }
+
+        [Test]
+        public void MyAny_should_return_true_if_source_contains_elements()
+        {
+            list.Add(1);
+            bool notEmpty = list.MyAny();
+            notEmpty.Should().BeTrue();
+        }
+
+        [Test]
+        public void MyAny_should_return_false_if_source_doesnt_contain_elements()
+        {
+            bool empty = list.MyAny();
+            empty.Should().BeFalse();
+        }
+
+        [Test]
+        public void Short_MyAny_should_throw_exception_if_source_is_null()
+        {
+            List<int>? nolist = null;
+            Action act = () => { var _ = nolist!.MyAny(); };
+            act.Should().Throw<ArgumentNullException>();
+
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_throw_exception_if_source_is_null()
+        {
+            List<int>? nolist = null;
+            Action act = () => nolist!.MyFirstOrDefault();
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_return_default_if_source_is_empty()
+        {
+      
+            int value = list.MyFirstOrDefault();
+            value.Should().Be(default);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with_custom_default_should_throw_exception_if_source_is_null()
+        {
+            List<int>? nolist = null;
+            Action act = () => nolist!.MyFirstOrDefault(3);
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with_custom_default_should_return_default_if_source_is_empty()
+        {
+            int value = list.MyFirstOrDefault(3);
+            value.Should().Be(3);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with_custom_default_and_predicate_should_throw_exception_if_source_is_null()
+        {
+            List<int>? nolist = null;
+            Action act = () => nolist!.MyFirstOrDefault(n => n < 0, 3);
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with__predicate_should_throw_exception_if_source_is_null()
+        {
+            List<int>? nolist = null;
+            Action act = () => nolist!.MyFirstOrDefault(n => n < 0);
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with__predicate_should_return_default_if_source_is_null()
+        {
+            int value = list.MyFirstOrDefault(n => n < 0);
+            value.Should().Be(default);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_return_the_first_value_if_there_is_one()
+        {
+            list.Add(1);
+            int first = list.MyFirstOrDefault();
+            first.Should().Be(1);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_return_default_if_theres_no_value()
+        {
+            int first = list.MyFirstOrDefault();
+            first.Should().Be(default);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_enumerator_should_return_values_in_correct_order()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            var enumerator = list.GetEnumerator();
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(1);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(2);
+
+            enumerator.MoveNext();
+            enumerator.Current.Should().Be(3);
+
+            enumerator.MoveNext().Should().BeFalse();
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with_custom_default_should_return_the_first_value_if_there_is_one()
+        {
+            list.Add(1);
+            int first = list.MyFirstOrDefault(3);
+            first.Should().Be(1);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with_custom_default_should_return_the_custom_default_value_if_there_is_one()
+        {
+            int first = list.MyFirstOrDefault(3);
+            first.Should().Be(3);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_return_the_first_value_matching_the_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+ 
+            int first = list.MyFirstOrDefault( n => n % 2 == 0);
+            first.Should().Be(2);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_return_the_default_value_if_none_match_the_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+ 
+            int first = list.MyFirstOrDefault( n => n < 0);
+            first.Should().Be(0);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_should_return_the_default_custom_value_if_none_match_the_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+ 
+            int first = list.MyFirstOrDefault( n => n < 0, 4);
+            first.Should().Be(4);
+        }
+        [Test]
+        public void MyFirstOrDefault_should_return_the_default_custom_value_if_the_source_is_empty_with_predicate()
+        {
+            int first = list.MyFirstOrDefault( n => n < 0, 4);
+            first.Should().Be(4);
+        }
+
+        [Test]
+        public void MyFirstOrDefault_with_custom_default_should_return_the_value_that_matches_the_predicate()
+        {
+            list.Add(1);
+            list.Add(2);
+            list.Add(-2);
+ 
+            int first = list.MyFirstOrDefault( n => n < 0, 4);
+            first.Should().Be(-2);
         }
     }
 }
